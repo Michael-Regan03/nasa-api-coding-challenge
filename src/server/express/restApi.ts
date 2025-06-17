e14d99fc4708aed2071973a6dd30305e51f81039
Michael Regan <michaeljamesregan03@gmail.com>
2025-06-17 09:35:39 +0100

    ✨ fetching apod data

--- src/server/express/restApi.ts
+++ src/server/express/restApi.ts
@@ -2,8 +2,13 @@ import packageJSON from "../../../package.json";
 import express, { Application } from "express";
 import cors from "cors";
 import { Request, Response } from "express";
+import { RespExampleType, ApodResponseType } from "@/typings/types";
+import axios from "axios";
 
 const app: Application = express();
+
+require("dotenv").config();
+
 
 app.use(express.json({ limit: "20mb" }));
 app.use(cors());
@@ -22,6 +27,33 @@ app.get(`/api/v1/version`, (req: Request, res: Response) => {
   };
   res.send(respObj);
 });
+
+const NASA_API_KEY = process.env.NASA_API_KEY;
+
+// Astronomy Picture of the Day
+app.get(`/api/v1/apod/:date?`, async (req: Request, res: Response) => {
+  try {
+    const date = req.params.date;
+    const nasaRes = await axios.get<ApodResponseType>(
+      "https://api.nasa.gov/planetary/apod",
+      {
+        params: {
+          api_key: NASA_API_KEY,
+          ...(date && { date }),
+        },
+      }
+    );
+
+    const respObj: ApodResponseType = nasaRes.data;
+    res.send(respObj);
+  } catch (error) {
+    console.error("Error fetching APOD:", error);
+    res.status(500).json({ error: "Failed to fetch APOD data" });
+  }
+});
+
+
+
 
 app.use(express.static("./.local/vite/dist"));
 
--- src/typings/types.d.ts
+++ src/typings/types.d.ts
@@ -3,3 +3,14 @@ type RespExampleType = {
   version: string;
   envVal: string;
 };
+
+export type ApodResponseType = {
+  date: string;
+  explanation: string;
+  hdurl: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string;
+};
+
