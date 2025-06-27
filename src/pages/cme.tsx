38d3b06008e11f0f50f5cf2c43eda276a37c681f
Michael-Regan03 <michaeljamesregan03@gmail.com>
2025-06-27 23:54:53 +0100

    💬 Adding content of flesh out pages

--- src/pages/cme.tsx
+++ src/pages/cme.tsx
@@ -5,6 +5,24 @@ import CMEForm from "@/components/cme/cmeForm";
 const HomePage = (): JSX.Element => {
   return (
     <div className={styles.app}>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold text-center mb-8 p mt-4">
+          Coronal Mass Ejection
+        </h1>
+        <p>
+          A coronal mass ejection (CME) is a significant ejection of plasma mass
+          from the Sun's corona into the heliosphere. CMEs are often associated
+          with solar flares and other forms of solar activity, but a broadly
+          accepted theoretical understanding of these relationships has not been
+          established.
+          <a
+            href="https://en.wikipedia.org/wiki/Coronal_mass_ejection"
+            className="text-blue-500 underline hover:text-blue-700"
+          >
+            source
+          </a>{" "}
+        </p>
+      </div>
       <CMEForm></CMEForm>
     </div>
   );
--- src/pages/neo/[id].tsx
+++ src/pages/neo/[id].tsx
@@ -25,8 +25,20 @@ export function NeoIDPage(): React.JSX.Element {
   if (loading) return <div>Loading...</div>;
 
   return (
-    <div>
-      <h1>{neo[0]?.name}</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold text-center mb-8 p mt-4">
+        Near Earth Object: {neo[0]?.name}
+      </h1>
+      <p>Absolute Magnitude: {neo[0]?.absolute_magnitude_h}</p>
+      <p>
+        Estimated Minimum Diameter:{" "}
+        {neo[0]?.estimated_diameter.kilometers.estimated_diameter_min}km
+      </p>
+      <p>
+        Estimated Maximum Diameter:{" "}
+        {neo[0]?.estimated_diameter.kilometers.estimated_diameter_max}km
+      </p>
+
       <NeoCloseApproachVisualizer neos={neo} flag={true} />
     </div>
   );
